34ab0e51b6b0c6b4611997410031e7e15bc9819b
Evan Johnson and Kate Mashokha <kate@>
2017-10-25 16:37:43 -0700

    implement functionality for users activity

--- app/src/main/java/com/epicodus/chatapp/activities/Chat.java
+++ app/src/main/java/com/epicodus/chatapp/activities/Chat.java
@@ -1,6 +1,19 @@
 package com.epicodus.chatapp.activities;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+import com.epicodus.chatapp.R;
+
+import butterknife.ButterKnife;
 
 public class Chat extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+        ButterKnife.bind(this);
+    }
 }
