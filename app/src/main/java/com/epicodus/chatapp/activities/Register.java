ccf9e0bfdd7c047330d0e7ec32477e0cfc965aa1
Evan Johnson and Kate Mashokha <kate@>
2017-10-26 11:19:17 -0700

    refactor to use firebase database over firebase.setcontext

--- app/src/main/java/com/epicodus/chatapp/activities/Register.java
+++ app/src/main/java/com/epicodus/chatapp/activities/Register.java
@@ -1,14 +1,141 @@
 package com.epicodus.chatapp.activities;
 
-import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.epicodus.chatapp.Constants;
 import com.epicodus.chatapp.R;
+import com.firebase.client.Firebase;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
-public class Register extends Activity {
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class Register extends AppCompatActivity {
+    EditText username, password;
+    Button registerButton;
+    String user, pass;
+    DatabaseReference mDataBase;
+
+    @OnClick(R.id.login)
+    public void loginButtonWasClicked(View v) {
+        startActivity(new Intent(Register.this, Login.class));
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        ButterKnife.bind(this);
+
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+        registerButton = (Button) findViewById(R.id.registerButton);
+
+//        Map<String, String> newUser = new HashMap<>();
+//        Map<String, String> newUserAndPassword = new HashMap<>();
+//
+//        newUser.put("russia", "russia");
+//        newUserAndPassword.put("password", "russia");
+//
+        mDataBase = FirebaseDatabase
+                .getInstance()
+                .getReference();
+
+//        FirebaseDatabase
+//                .getInstance()
+//                .getReference("users/russia")
+//                .setValue(newUserAndPassword);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                user = username.getText().toString();
+                pass = password.getText().toString();
+
+                if (user.equals("")) {
+                    username.setError("can't be blank");
+                } else if (pass.equals("")) {
+                    password.setError("can't be blank");
+                } else if (!user.matches("[A-Za-z0-9]+")) {
+                    username.setError("only alphabet or number allowed");
+                } else if (user.length() < 5) {
+                    username.setError("at least 5 characters long");
+                } else if (pass.length() < 5) {
+                    password.setError("at least 5 characters long");
+                } else {
+                    final ProgressDialog pd = new ProgressDialog(Register.this);
+                    pd.setMessage("Loading...");
+                    pd.show();
+
+
+
+
+
+
+
+
+
+
+                    StringRequest request = new StringRequest(Request.Method.GET, Constants.FIREBASE_URL + "/userNames.json", new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String s) {
+
+
+                            if (s.equals("null")) {
+//                                reference.child(user).child("password").setValue(pass);
+                                Toast.makeText(Register.this, "registration successful", Toast.LENGTH_LONG).show();
+                            } else {
+                                try {
+                                    JSONObject obj = new JSONObject(s);
+
+                                    if (!obj.has(user)) {
+//                                        reference.child(user).child("password").setValue(pass);
+                                        Toast.makeText(Register.this, "registration successful", Toast.LENGTH_LONG).show();
+                                    } else {
+                                        Toast.makeText(Register.this, "username already exists", Toast.LENGTH_LONG).show();
+                                    }
+
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+
+                            pd.dismiss();
+                        }
+
+                    }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError volleyError) {
+                            System.out.println("" + volleyError);
+                            pd.dismiss();
+                        }
+                    });
+
+                    RequestQueue rQueue = Volley.newRequestQueue(Register.this);
+                    rQueue.add(request);
+                }
+            }
+        });
     }
 }
